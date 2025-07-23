241a9ffb32e50a8130f80f8ddea145ea3ca9b7b3
VDikshita <Dikshita_v@anantsol.com>
2025-07-23 17:13:00 +0530

    ustr craper code

--- scraper/ustr_scraper.py
+++ scraper/ustr_scraper.py
@@ -20,6 +20,37 @@ def fetch_press_releases():
             full_url=BASE_URL + href
             article_links.append((title, full_url))
 
-    return article_links        
+    return article_links 
+
+
+def extract_article_text(url):
+    response=requests.get(url)
+    soup=BeautifulSoup(response.content,"lxml")
+    paragraphs=soup.select("div.field--name-body p")
+    return "n".join(p.get_text(strip=True)for p in paragraphs)
+
+
+def run_scraper():
+    articles=[]
+    for title, url in fetch_press_releases():
+        print(f"Scrapping:{title}")
+        content=extract_article_text(url)
+        articles.append({
+            "title": title,
+            "url": url,
+            "content": content,
+            "scraped-at": datetime.utcnow().isoformat()
+        })
+
+    os.makedirs("outputs", exist_ok=True)
+    with open("outputs/ustr_press_releases.json", "w", encoding="utf-8") as f:
+        json.dump(articles,f, indent=2, ensure_ascii=False)
+    print(f"Saved {len(articles)} articles to outputs/ustr_press_releases.json")  
+
+if __name__ == "__main__":
+    run_scraper()
+
+
+     
 
         
